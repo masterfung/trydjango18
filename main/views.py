645243a7620749b0b870f0d0833947d7ee3f2f63
Tsung Hung <thung@me.com>
2015-06-22 15:59:44 -0700

    added post to the views

--- main/views.py
+++ main/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {})
 
 def about(request):
     return render(request, 'about.html', {})
 
+def post(request):
+    return render(request, 'post.html', {})
+
 def contact(request):
     return render(request, 'contact.html', {})
+
